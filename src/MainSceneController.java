590e095a287491871e7f5533c50381e3f495cffc
abuubidawadi <abuubida22@gmail.com>
2026-02-08 00:46:51 -0800

    ok

--- src/MainSceneController.java
+++ src/MainSceneController.java
@@ -1,20 +1,80 @@
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 public class MainSceneController {
 
     @FXML
-    private TextField tfTitle;
+    private Text ainoggo_home;
 
     @FXML
-    void btnOKclicked(ActionEvent event) {
-        Stage mainWindow = (Stage) tfTitle.getScene().getWindow();
-        String title = tfTitle.getText();
-        mainWindow.setTitle(title);
-    }
+    private Button login_button;
+
+    @FXML
+    private Button login_button_reg_page;
+
+    @FXML
+    private Hyperlink login_forgot_password;
+
+    @FXML
+    private AnchorPane login_page_form;
+
+    @FXML
+    private AnchorPane login_page_header;
+
+    @FXML
+    private AnchorPane login_page_img;
+
+    @FXML
+    private TextField login_username;
+
+    @FXML
+    private Button reg_button_log_page;
+
+    @FXML
+    private CheckBox reg_checkbox;
+
+    @FXML
+    private PasswordField reg_conf_pass;
+
+    @FXML
+    private TextField reg_email;
+
+    @FXML
+    private TextField reg_name;
+
+    @FXML
+    private AnchorPane reg_page_form;
+
+    @FXML
+    private AnchorPane reg_page_header;
+
+    @FXML
+    private ImageView reg_page_img;
+
+    @FXML
+    private PasswordField reg_pass;
+
+    @FXML
+    private TextField reg_username;
+
+    @FXML
+    private Button register_button;
+
+    @FXML
+    private Hyperlink switch_to_lawyer_login;
+
+    @FXML
+    private Hyperlink switch_to_lawyer_reg;
+
+
 
 }
 
