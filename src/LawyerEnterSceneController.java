c235a5b76345253d92a2a31d3956e5f6eec5e403
abuubidawadi <abuubida22@gmail.com>
2026-02-11 14:49:57 -0800

    ok

---
+++ src/LawyerEnterSceneController.java
@@ -0,0 +1,161 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class LawyerEnterSceneController {
+
+    @FXML
+    private ImageView ainoggo_home_logo_2nd_page;
+
+    @FXML
+    private ImageView ainoggo_home_logo_log;
+
+    @FXML
+    private ImageView ainoggo_home_logo_reg;
+
+    @FXML
+    private Text ainoggo_home_text_2nd_page;
+
+    @FXML
+    private Text ainoggo_home_text_log;
+
+    @FXML
+    private Text ainoggo_home_text_reg;
+
+    @FXML
+    private TextField bar_reg_no;
+
+    @FXML
+    private TextField consult_fee;
+
+    @FXML
+    private TextField exp_yr;
+
+    @FXML
+    private TextField law_firm;
+
+    @FXML
+    private Button login_button;
+
+    @FXML
+    private Button login_button_reg_page;
+
+    @FXML
+    private Hyperlink login_forgot_password;
+
+    @FXML
+    private AnchorPane login_page;
+
+    @FXML
+    private AnchorPane login_page_form;
+
+    @FXML
+    private AnchorPane login_page_header;
+
+    @FXML
+    private AnchorPane login_page_img;
+
+    @FXML
+    private PasswordField login_pass;
+
+    @FXML
+    private TextField login_username;
+
+    @FXML
+    private TextField office_address;
+
+    @FXML
+    private Button proceed_button;
+
+    @FXML
+    private Button reg_button_log_page;
+
+    @FXML
+    private CheckBox reg_checkbox;
+
+    @FXML
+    private PasswordField reg_conf_pass;
+
+    @FXML
+    private TextField reg_email;
+
+    @FXML
+    private TextField reg_name;
+
+    @FXML
+    private AnchorPane reg_page;
+
+    @FXML
+    private AnchorPane reg_page1;
+
+    @FXML
+    private AnchorPane reg_page_form;
+
+    @FXML
+    private AnchorPane reg_page_header;
+
+    @FXML
+    private AnchorPane reg_page_img;
+
+    @FXML
+    private PasswordField reg_pass;
+
+    @FXML
+    private TextField reg_username;
+
+    @FXML
+    private Button register_button;
+
+    @FXML
+    private AnchorPane second_page_form;
+
+    @FXML
+    private AnchorPane second_page_header;
+
+    @FXML
+    private ComboBox<?> spec_area;
+
+    @FXML
+    private Hyperlink switch_to_client_login1;
+
+    @FXML
+    private Hyperlink switch_to_client_reg;
+
+    @FXML
+    private Hyperlink switch_to_client_reg2;
+
+    @FXML
+    private Button upload_button;
+
+    @FXML
+    private ImageView uploaded_img;
+
+    @FXML
+    void forgotPassword(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registration(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchbetweenloginandreg(ActionEvent event) {
+
+    }
+
+}
