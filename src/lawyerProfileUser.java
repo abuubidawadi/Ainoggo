651565741e2655b40c56e4a69c24571d42d723be
abuubidawadi <abuubida22@gmail.com>
2026-02-20 11:50:04 -0800

    okkk

---
+++ src/lawyerProfileUser.java
@@ -0,0 +1,66 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class lawyerProfileUser {
+    @FXML
+    private ImageView ainoggo_home_logo_log;
+
+    @FXML
+    private Text ainoggo_home_text_log;
+
+    @FXML
+    private Text bar_reg_no;
+
+    @FXML
+    private Text bio;
+
+    @FXML
+    private Button book_btn;
+
+    @FXML
+    private Button dashboard_btn;
+
+    @FXML
+    private DatePicker date_picker;
+
+    @FXML
+    private Text exp_yr;
+
+    @FXML
+    private Text fee;
+
+    @FXML
+    private Text law_firm;
+
+    @FXML
+    private ImageView lawyer_img;
+
+    @FXML
+    private Text lawyer_name;
+
+    @FXML
+    private AnchorPane login_page;
+
+    @FXML
+    private AnchorPane login_page_header;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private Text mail;
+
+    @FXML
+    private Text office_address;
+
+    @FXML
+    private Text specialization;
+
+    @FXML
+    private ComboBox<?> time_picker;
+}
